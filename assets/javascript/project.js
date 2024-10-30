10a427cd7f72a6fd5f762e7174b18f0a820c66f1
VictoriousMango <additya.mishra20@st.niituniversity.in>
2024-10-30 23:53:18 +0530

    Modified card generation

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -6,39 +6,31 @@
 // FrameWorksShowSkill
 
 skills = {
-    "CartogramAssessment" : {
-        "BackEndShowSkill" : ["Python"],
-        "DataBaseShowSkill" : ["Deta", "No-SQL"],
-        "FrontEndShowSkill" : ["Streamlit"],
-        "CloudServicesShowSkill" : ["Google Collab"],
-        "MachineLearningShowSkill" : [],
-        "FrameWorksShowSkill" : []
+    "CartogramAssessment": {
+        "Back-End": ["Python"],
+        "Database": ["Deta", "No-SQL"],
+        "Front-End": ["Streamlit"],
+        "Cloud Services": ["Google Collab"]
     },
-    "CCA" : {
-        "BackEndShowSkill" : ["Python"],
-        "DataBaseShowSkill" : [],
-        "FrontEndShowSkill" : ["HTML", "CSS", "Bootstrap"],
-        "CloudServicesShowSkill" : [],
-        "MachineLearningShowSkill" : ["Random Forest", "Linear Regression"], //, "Logistic Regression", "SVM"
-        "FrameWorksShowSkill" : ["Flask"]
+    "CCA": {
+        "Back-End": ["Python"],
+        "Front-End": ["HTML", "CSS", "Bootstrap"],
+        "Machine Learning": ["Random Forest", "Linear Regression"], //, "Logistic Regression", "SVM"
+        "Frameworks": ["Flask"]
     }
 }
 
 function ShowSkills(project) {
-    console.log(project);
-    var skillContent= "";
-    for (cardName in skills[project]){
-        skillContent= "";
-        for (skill in skills[project][cardName]){
-            console.log(cardName + " : " + skills[project][cardName][skill]);
+    var skillContent = "";
+    document.getElementById("skillsList").innerHTML = "";
+    for (cardName in skills[project]) {
+        skillContent = "";
+        for (skill in skills[project][cardName]) {
             skillContent += `<li>${skills[project][cardName][skill]}</li>`
-            
+
         }
-        if (skillContent != ""){
-            document.getElementById(cardName).innerHTML = skillContent;
-        }
-        else {
-            document.getElementById(cardName).innerHTML = "";
-        }
+
+        let skillCard=`<div class="col"><div class="card"><h5 class="card-title"> ${cardName}</h5><hr><div class="card-body">${skillContent}</div></div></div>`
+        document.getElementById("skillsList").innerHTML += skillCard;
     }
 }
